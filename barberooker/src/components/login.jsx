979ed26728a480c0e1bd6365168918334f877b35
KRISHNAKUMAR-N-04 <krishnakumarnatarajan04.com>
2024-11-05 11:25:10 +0530

    Login styled and routed

--- barberooker/src/components/login.jsx
+++ barberooker/src/components/login.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 import api from '../api';
+import { useNavigate } from 'react-router-dom';
 import '../components/Login.css'
 
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const res = await api.post('/login', { username, password });
+            navigate('./Signup.jsx')
             alert('Login Successful');
+            
         } catch {
-            alert('Login Failed');
+            alert('invalid credentials');
         }
     };
 
